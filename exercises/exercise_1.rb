d608c6dbdea0c1359980ee86ebfae0f27974080c
jaklahaulps <liu.perry@gmail.com>
2019-01-15 22:10:07 +0000

    exercise two complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,5 +9,6 @@ Buraby = Store.create(name:"Buraby", annual_revenue: 300000 , mens_apparel: [tru
 Richmond = Store.create(name:"Richmond", annual_revenue: 12600000 , mens_apparel: [false], womens_apparel: [true])
 Gastown = Store.create(name:"Gastown", annual_revenue: 190000 , mens_apparel: [true], womens_apparel: [false])
 
-users = Store.all
-puts users.count
+@stores = Store.all
+
+puts @stores.count
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# stores = Store.all
+@store1 = Store.find_by(id: 1)
+@store2 = @stores[1]
+@store1.name = 'Toronto'
+
+puts  "store1 and store2 display 01:  #{@store1.name} and  #{@store2.name} "
+puts  "store1 and store2 display 02:  #{@stores[2].name} and  #{@stores[1].name} "
+#note @store1 doesn't use @stores from first exercise.
